641099dc8d4ac4ee95fe632c6ec1854886ad0363
Yury Barysevich1 <kassik0boks@gmail.com>
2023-05-31 13:30:11 +0300

    finish task 8

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,24 @@ import { AppController } from './app.controller';
 import { CartModule } from './cart/cart.module';
 import { AuthModule } from './auth/auth.module';
 import { OrderModule } from './order/order.module';
+import { ConfigModule } from '@nestjs/config';
+import { PostgresModule } from 'nest-postgres';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PostgresModule.forRoot({
+      host: process.env.PG_HOST,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      user: process.env.PG_USERNAME,
+      port: +process.env.PG_PORT,
+    }),
     AuthModule,
     CartModule,
     OrderModule,
   ],
-  controllers: [
-    AppController,
-  ],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
--- src/cart/cart.module.ts
+++ src/cart/cart.module.ts
@@ -5,10 +5,10 @@ import { OrderModule } from '../order/order.module';
 import { CartController } from './cart.controller';
 import { CartService } from './services';
 
-
 @Module({
-  imports: [ OrderModule ],
-  providers: [ CartService ],
-  controllers: [ CartController ]
+  imports: [OrderModule],
+  providers: [CartService],
+  controllers: [CartController],
+  exports: [CartService],
 })
 export class CartModule {}
